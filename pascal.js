ea2c6522f4c7192c3f520d8a1df705619e786d10
Jtmaurya <49449408+Jtmaurya@users.noreply.github.com>
2020-03-20 11:04:21 +0530

    Update pascal.js

--- pascal.js
+++ pascal.js
@@ -7,7 +7,6 @@
             var arr = [];
             var tmp;
             for(var i=0;i<n;i++){
-                //Each element in array is in turn an array
                 // The index is the row number and the array values are the row values
                 arr[i]=[];
                 for(var j=0; j<=i; j++){
@@ -15,7 +14,6 @@
                     if(j==i){
                         arr[i].push(1);
                     }else{
-                        //The following line adds up the two numbers directly above this element.
                         tmp = (!!arr[i-1][j-1]?arr[i-1][j-1]:0)+(!!arr[i-1][j]?arr[i-1][j]:0);
                         arr[i].push(tmp);
                     }
